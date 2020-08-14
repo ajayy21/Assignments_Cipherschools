978e73585ca9061aed525bc06d29b6ff4b56716d
Ajay Singh Shekhawat <shekhawat.ajay16@gmail.com>
2020-08-14 18:29:27 +0530

    easy2 done

---
+++ Assignment1/Very Easy Questions/question2.js
@@ -0,0 +1,13 @@
+let num1=4;
+let num2=5;
+divide=(num3,num4)=>{
+    if(num1%num2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+ 
+
+}
+console.log(divide(num1,num2));
