7d5528c933e044a8b7e5dcc5a597cfad27a1a58d
Ajay Singh Shekhawat <shekhawat.ajay16@gmail.com>
2020-08-14 20:03:13 +0530

    Medium 2 done

---
+++ Assignment1/Medium Questions/question2.js
@@ -0,0 +1,9 @@
+testjackpot=(array)=>{
+    for (let i = 0; i < array.length-1; i++) {
+        if(array[i]!==array[i+1]){
+            return false
+        }
+    }
+    return true 
+}
+console.log(testjackpot(["abc","abc","abc","abc"]))
