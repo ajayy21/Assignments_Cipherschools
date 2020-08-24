7bf5c56f90962d58e11996dbb3abbebd33978afc
Ajay Singh Shekhawat <shekhawat.ajay16@gmail.com>
2020-08-24 20:23:40 +0530

    Assignment 3

--- Assignment3/src/components/Navbar.js
+++ Assignment3/src/components/Navbar.js
@@ -1,39 +1,19 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react'
+import './Navbar.css'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color='secondary'>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            TODO APP
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div class="navbar">
+            <p id="Gryffindor" onClick={() => {
+                return this.props.filte("Important")
+            }}>Important</p>
+            <p id="General" onClick={() => {
+                return this.props.filte("General")
+            }}>General</p>
+        
+            </div>
+        )
+    }
 }
+export default Navbar;
