8e187bd00aa79333060489d47cde9608ee3551a0
Ajay Singh Shekhawat <shekhawat.ajay16@gmail.com>
2020-08-24 20:24:37 +0530

    Assignment 3

--- Assignment3/src/components/Display.js
+++ Assignment3/src/components/Display.js
@@ -1,65 +1,37 @@
-import React, { Component } from 'react';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import IconButton from '@material-ui/core/IconButton';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
-export default class Display extends Component {
-    
-constructor(props){
-    super(props)
-    
+import React from 'react'
+import Loading from './Loading'
+import './Display.css'
+class Display extends React.Component{
+    // return (
+    //     props.coursedetails.map(element=>{
+    //     return(
+    //         <div>
+    //         <h3>Title :{element.title}</h3>
+    //         <p>Instructor :{element.Instructor}</p>  
+    //         <p>Details :{element.Details}</p>
+    //         </div>
+    //     )
+    // })
+    // )
+    handlerOnclick = (event) => {
+        return this.props.delete(event.target.id)
+    }
+    render(){
+        const courselist = this.props.coursedetails.length?this.props.coursedetails.map((element)=>{
+            return(
+                <div class="course-display" key={element.id}>
+                 <h3>Title :{element.title}</h3>
+                 <h5>Details :{element.details}</h5>
+                 <button id={element.id} onClick={this.handlerOnclick}>Delete</button>
+                 </div>
+            )
+        }) : (<Loading/>)
+        return(
+            <div>
+                {courselist}
+            </div>
+        )
+    }
 }
 
-edit=(entry)=>{
-    var pos;
-    this.props.data.forEach((element,index) => {
-        if(element==entry){
-            pos=index;
-        }
-    });
-    var person = prompt("Please enter your updated Chore", "Enter Here");
-    this.props.data[pos]=person;
-    console.log(this.props.data)
-    this.props.edititem(this.props.data)
-    
-
-}
-
-
-delete=(entry)=>{
-    let newarray=this.props.data.filter((item)=>{
-        return item!=entry;
-    })
-    this.props.deleteitem(newarray)
-}
-
-render(){
-    return (<>
-  
-      {this.props.data.map((entry) => { 
-          return <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography >{entry}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <IconButton aria-label="add" onClick={()=>{return this.delete(entry)}}>
-                <DeleteOutlinedIcon size='large' color ='secondary'/>
-            </IconButton>
-            <IconButton aria-label="add" onClick={()=>{return this.edit(entry)}}>
-                <EditOutlinedIcon size='large' color ='secondary'/>
-            </IconButton>
-            
-          </AccordionDetails>
-        </Accordion>
-      })}
-   
-  </>)}
-}
+export default Display;
