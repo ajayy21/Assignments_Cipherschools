db436670a61971969b10343b014c2861b3368276
Ajay Singh Shekhawat <shekhawat.ajay16@gmail.com>
2020-08-30 18:40:56 +0530

    Final portfolio

---
+++ React-Portfolio/src/components/aboutme.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+    <div><h1>I am a B-tech student from lovely Professional University.  !</h1></div>
+    )
+  }
+}
+
+export default About;
